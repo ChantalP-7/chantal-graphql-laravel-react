40d95e11829ea00beeeeaa1a37d2aed924de4853
pmoukda <moukda_p@hotmail.com>
2025-12-05 07:38:26 -0500

    Appel de la fonction GetUsager

--- frontend/src/pages/Catalogue.jsx
+++ frontend/src/pages/Catalogue.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getproduits } from "../api/produits";
 import { Link } from "react-router-dom";
 import Filtre from "../components/Filtre";
+import GetUsager from "../components/GetUsager";
 
 
 /**
@@ -17,7 +18,7 @@ const [filtre, setFiltre] = useState({ identite: null, pays: null });
 const [ordre, setOrdre] = useState("");
 
 // Obtenir les infos de l'usager 
-const user = JSON.parse(localStorage.getItem("user"));
+const user = GetUsager();
  
 const bouteillesParPage = 12;
 
@@ -74,7 +75,7 @@ const bouteillesParPage = 12;
 						<Link className="block w-full" key={p.id} to={`/produit/${p.id}`}>			
 							<button className="bouton-accent w-full flex items-center justify-center gap-2 " id="ajoutBouteille">
 								Voir détail
-								<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#fff"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+								<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#fff"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
 								</svg>
 							</button>
 						</Link>
